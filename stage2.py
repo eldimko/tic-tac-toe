1c4a952ca720c3d7a8789bd90921979b9875e5ca
Dmitry Kolesnik <67832270+eldimko@users.noreply.github.com>
2020-07-23 00:48:45 +0200

    Create stage2.py

---
+++ stage2.py
@@ -0,0 +1,8 @@
+ttt_input = input("Enter cells: ")
+ttt_list = [sign for sign in ttt_input]
+
+print("---------")
+print("| {} {} {} |".format(ttt_list[0], ttt_list[1], ttt_list[2]))
+print("| {} {} {} |".format(ttt_list[3], ttt_list[4], ttt_list[5]))
+print("| {} {} {} |".format(ttt_list[6], ttt_list[7], ttt_list[8]))
+print("---------")
